9743fc4c88a3e2ffa71924226e70180eb0993af1
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-02-20 05:31:26 +0530

    Easy

---
+++ 1. Two Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr=new int[2];
+      for(int i=1;i<nums.length;i++){
+          for(int j=i;j<nums.length;j++){
+          if(nums[j-i]+nums[j]==target) {
+          arr[0]=j-i;
+          arr[1]=j;
+          return arr;
+          }
+      }  
+      }  
+      return new int[0];
+        
+    }
+}
