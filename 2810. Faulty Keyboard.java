5c2c8fd02af6bf7c39b28fba599d6ad43c3b208f
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2025-01-03 10:57:45 +0530

    Easy

---
+++ 2810. Faulty Keyboard.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder sb=new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'i') {
+                sb=sb.reverse();
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+        
+    }
+}
