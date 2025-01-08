a13bd842ae46d0e53aeabd1b43d49b1369695033
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2025-01-08 11:34:06 +0530

    Easy

---
+++ 1528. Shuffle String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        int n=indices.length;
+        char[] arr=new char[n];
+        for(int i=0;i<n;i++){
+            arr[indices[i]]=s.charAt(i);
+        }
+        return String.valueOf(arr);
+    }
+}
