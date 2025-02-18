903c477f15fe577764c666a5a98a8ec90d725967
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2025-02-18 21:34:06 +0530

    Medium

--- 442. Find All Duplicates in an Array(0ms).java
+++ 442. Find All Duplicates in an Array(0ms).java
@@ -2,18 +2,15 @@ class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         return new CustomList(nums);
     }
-
     class CustomList extends ArrayList<Integer> {
         int nums[];
         CustomList(int[] nums) {
             this.nums = nums;
         }
-
         public int size() {
             customListMethod();
             return super.size();
         }
-
         public void customListMethod() {
             int size = nums.length;
             int[] temp = new int[size+1];
