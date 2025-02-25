0189f45c7b9f6b21fa646deb1fb0c7fffda51c38
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2025-02-25 10:19:29 +0530

    Easy

---
+++ 461. Hamming Distance(0ms).java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        while(x > 0 || y > 0){
+            count += ((x & 1) ^ (y & 1));
+            x >>= 1;
+            y >>= 1;
+        }
+        return count;
+    }
+}
