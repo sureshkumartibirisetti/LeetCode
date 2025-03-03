3edf2a17bf0089dcaa81105762adfc492c32830b
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2025-03-03 17:37:20 +0530

    Easy

---
+++ 2154. Keep Multiplying Found Values by Two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == original){
+                return findFinalValue(nums, original*2);
+            }
+        }
+        return original;
+    }
+}
