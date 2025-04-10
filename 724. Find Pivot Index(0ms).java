958b8e98f977b10097e0a75d868c70bb23e08212
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2025-04-10 12:40:26 +0530

    Easy

---
+++ 724. Find Pivot Index(0ms).java
@@ -0,0 +1,14 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int leftsum=0,rightSum=0;
+        for(int i=0;i<nums.length;i++){
+            rightSum+=nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            rightSum-=nums[i];
+            if(rightSum==leftsum) return i;
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+}
