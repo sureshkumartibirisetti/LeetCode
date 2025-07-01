b9aa5bc8e1c7bb1e76ffb451b642144be12e0f02
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2025-07-01 11:52:15 +0530

    Time: 0 ms (100%), Space: 40.6 MB (42.8%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,14 +1,5 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int res=0;
-        for(int i=1;i<=num/2;i++){
-            if(num%i==0){
-                res+=i;
-            }
-        }
-        if(res==num){
-            return true;
-        }
-        return false;
+        return (num==6 || num==28 || num==496 || num==8128 || num==33550336);
     }
 }
