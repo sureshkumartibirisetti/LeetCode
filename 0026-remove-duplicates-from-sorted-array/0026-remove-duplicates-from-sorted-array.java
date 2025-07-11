e69c9c58113267b1741d7ad6b8b8701e2267d7ad
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2025-07-11 15:53:37 +0530

    Time: 1 ms (59.94%), Space: 44.5 MB (96.2%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j=1;
+        for(int i=1;i<nums.length;i++){
+            if (nums[i]!=nums[i-1]){
+                 nums[j]=nums[i];
+                 j+=1;
+            }
+
+        }
+        return j;
+        
+    }
+}
+//  def removeDuplicates(self, nums: List[int]) -> int:
+//         j=1
+//         for i in range(1,len(nums)):
+//             if nums[i]!=nums[i-1]:
+//                 nums[j]=nums[i]
+//                 j+=1
+//         return j
